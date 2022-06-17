fd417564002f0c8b3801d308c3d1d2b13105045a
kirstgonz <104031234+kirstgonz@users.noreply.github.com>
2022-06-16 20:57:30 -0500

    updated index.html and added some notes

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+
+api.datamuse.com/words
+api.datamuse.com/sug //autocomplete function on search input fields
+
+/*
+rhy	-Rhymes ("perfect" rhymes, per RhymeZone)	spade → aid
+nry	-Approximate rhymes (per RhymeZone)	forest → chorus
+hom	-Homophones (sound-alike words)	course → coarse
+cns	-Consonant match
+syn	-Synonyms (words contained within the same WordNet synset)
+*/
